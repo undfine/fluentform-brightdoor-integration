72afd1cfe2c787cad9bbee8d53fcaad1117c8dba
Dustin W <un.dfine@gmail.com>
2023-08-15 11:34:19 -0400

    Fix classname conflict on previous version

--- fluentform-brightdoor-integration.php
+++ fluentform-brightdoor-integration.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Fluent Forms Brightdoor Integration
  * Description: Used to import and sync contacts with Brightdoor CRM
- * Version:     1.2.1
+ * Version:     1.2.2
  * Author:      Dustin Wight
  * Author URI: https://github.com/undfine/fluentform-brightdoor-integration
  * Text Domain: ff_brightdoor
@@ -18,14 +18,26 @@ add_action('init', function (){
         add_action( 'admin_notices', function(){
             
             $message = '<p>' . esc_html__( 'FluentForm Brightdoor Integration Add-On Requires FluentForm', 'ff_brightdoor' ) . '</p>';
-
             print_error( $message );
             
         });
         return;
     }
-    require plugin_dir_path( __FILE__ ) . 'includes/BrightDoor.php';
-    require plugin_dir_path( __FILE__ ) . 'includes/BrightDoorApi.php';
     
-    $plugin = new FF_BrightDoor();
+    // Check that class doesn't already exist
+    if( !class_exists('FF_BrightDoor')){
+
+        require plugin_dir_path( __FILE__ ) . 'includes/BrightDoor.php';
+        require plugin_dir_path( __FILE__ ) . 'includes/BrightDoorApi.php';
+        
+        $plugin = new FF_BrightDoor();
+    
+    } else {
+        add_action( 'admin_notices', function(){
+            
+            $message = '<p>' . esc_html__( 'The FF Brightdoor Class is already in use, please deactivate or uninstall the conflicting plugin', 'ff_brightdoor' ) . '</p>';
+            print_error( $message );
+            
+        });
+    }
 });
