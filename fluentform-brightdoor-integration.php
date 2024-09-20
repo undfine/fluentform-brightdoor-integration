db657ecdf05df5ee9bfe79c75600a9c5df13b160
Dustin W <un.dfine@gmail.com>
2024-09-20 10:22:07 -0400

    fixed error message if main plugin is missing

--- fluentform-brightdoor-integration.php
+++ fluentform-brightdoor-integration.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Fluent Forms Brightdoor Integration
  * Description: Used to import and sync contacts with Brightdoor CRM
- * Version:     1.2.4
+ * Version:     1.2.5
  * Author:      Dustin Wight
  * Author URI: https://github.com/undfine/fluentform-brightdoor-integration
  * Text Domain: ff_brightdoor
@@ -17,8 +17,8 @@ add_action('init', function (){
     if (!defined('FLUENTFORM') ) {
         add_action( 'admin_notices', function(){
             
-            $message = '<p>' . esc_html__( 'FluentForm Brightdoor Integration Add-On Requires FluentForm', 'ff_brightdoor' ) . '</p>';
-            print_error( $message );
+            $message = esc_html__( 'FluentForm Brightdoor Integration Add-On Requires FluentForm', 'ff_brightdoor' );
+            echo '<div class="notice notice-success is-dismissible"><p>'.$message.'</p></div>';
             
         });
         return;
@@ -35,8 +35,8 @@ add_action('init', function (){
     } else {
         add_action( 'admin_notices', function(){
             
-            $message = '<p>' . esc_html__( 'The FF Brightdoor Class is already in use, please deactivate or uninstall the conflicting plugin', 'ff_brightdoor' ) . '</p>';
-            print_error( $message );
+            $message = esc_html__( 'The FF Brightdoor Class is already in use, please deactivate or uninstall the conflicting plugin', 'ff_brightdoor' );
+            echo '<div class="notice notice-success is-dismissible"><p>'.$message.'</p></div>';
             
         });
     }
